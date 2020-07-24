9a2f0d93f0adecbe1af058035b42edbcb4bccd54
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-24 15:49:47 +0100

    Removed some of the CSS Background Placeholders and moved the news axios get request up a level to the Landing component

--- src/components/columns/LeftColumn.jsx
+++ src/components/columns/LeftColumn.jsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Columns.css'
-import axios from 'axios'
 import { LeftNewsCard } from '../index'
 
-const LeftColumn = () => {
-  const [news, setNews] = useState(null)
-
-  useEffect(() => {
-    axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.REACT_APP_API_KEY)
-      .then(response => {
-        console.log('Successful response', response)
-        setNews(response.data.articles)
-      })
-      .catch(error => {
-        console.log('There was an error processing this request', error)
-      })
-  }, [])
+const LeftColumn = ({news}) => {
   return (
     <div className='left-column-container'>
-      {news && news.map(newsArticle => {
+      {news && news.slice(0, 2).map(newsArticle => {
         return(
           <LeftNewsCard newsArticle={newsArticle}/>
         )
--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Landing.css'
+import axios from 'axios'
 import { LeftColumn, CenterColumn, RightColumn } from '../index'
 
 const Landing = () => {
+  const [news, setNews] = useState(null)
+
+  useEffect(() => {
+    axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.REACT_APP_API_KEY)
+      .then(response => {
+        console.log('Successful response', response)
+        setNews(response.data.articles)
+      })
+      .catch(error => {
+        console.log('There was an error processing this request', error)
+      })
+  }, [])
 
   return (
     <div className='landing-container'>
-      <LeftColumn />
-      <CenterColumn />
-      <RightColumn />
+      <LeftColumn news={news}/>
+      <CenterColumn news={news}/>
+      <RightColumn news={news}/>
     </div>
   )
 }
