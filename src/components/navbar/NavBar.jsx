92b38e6f988c8be1bcbc8a32b8e5e04e57e112b7
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-07-24 13:44:12 +0100

    Small changes to Navbar component and added the font family

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './NavBar.css'
 
 const NavBar = () => {
+  const navbarLinks = ['Today', 'Topics', 'Writers', 'Podcasts', 'Magazine', 'More', 'Search', 'Login']
   return (
     <div data-testid='navbar' className='navbar-container'>
-      <div data-testid='navbarTitle'>
-        The Spectator
+      <div data-testid='navbar-title' className='navbar-title'>
+        <h1>The Spectator</h1>
       </div> 
+      <div className='navbar-links'>
+        {navbarLinks.map((navbarLink) => {
+          return (<a href="/">{navbarLink}</a>)
+        })}
+      </div>
     </div>
   )
 }
